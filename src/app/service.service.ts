1be8f0435be5491e27620ac71c142d4d3f52b795
ramthulla9954 <ramthulla9866@gmail.com>
2018-09-25 16:51:05 +0530

    ramthulla
    service changes

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -7,13 +7,25 @@ import { CanActivate, Router } from '@angular/router';
   providedIn: 'root'
 })
 export class ServiceService implements CanActivate {
-  urlLogin = 'http://103.263.168.73.3000/login';
-
-  constructor(private objHTTP: HttpClient, private objrouter: Router) { }
+    urlLogin = 'http://103.263.168.73.3000/login';
+    urltask='http://103.253.168.73.3000/task/list';
+    urlsalesreport='http://103.253.168.73.3000/sales/reports';
+    urlcafstatus='http://103.253.168.73.3000/caf/status';
+constructor(private objHTTP: HttpClient, private objrouter: Router) { }
   login(frmLoginObj) {
     return this.objHTTP.post(this.urlLogin, frmLoginObj).pipe(map(res => res));
   }
-  canActivate() {
+  task(frmtaskObj) {
+    return this.objHTTP.post(this.urltask, frmtaskObj).pipe(map(res => res));
+  }
+  salesreport(frmsalesreportObj) {
+    return this.objHTTP.post(this.urlsalesreport, frmsalesreportObj).pipe(map(res => res));
+  }
+  cafstatus(frmcafstatusObj) {
+    return this.objHTTP.post(this.urlcafstatus, frmcafstatusObj).pipe(map(res => res));
+  }
+    
+   canActivate() {
     console.log(localStorage.getItem('U_Id' ));
     if (localStorage.getItem('U_Id' ) !== null) {
       this.objrouter.navigate(['']);
