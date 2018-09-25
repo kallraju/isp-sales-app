845250cd54bdf73f5a3924c3cf742a1dd1469525
Vara Prasad <varap9533@outlook.com>
2018-09-25 17:03:48 +0530

    vara
    we have change :

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -7,10 +7,10 @@ import { CanActivate, Router } from '@angular/router';
   providedIn: 'root'
 })
 export class ServiceService implements CanActivate {
-    urlLogin = 'http://103.263.168.73.3000/login';
-    urltask='http://103.253.168.73.3000/task/list';
-    urlsalesreport='http://103.253.168.73.3000/sales/reports';
-    urlcafstatus='http://103.253.168.73.3000/caf/status';
+    urlLogin = 'http://103.263.168.73:3000/login';
+    urltask='http://103.253.168.73:3000/task/list';
+    urlsalesreport='http://103.253.168.73:3000/sales/reports';
+    urlcafstatus='http://103.253.168.73:3000/caf/status';
 constructor(private objHTTP: HttpClient, private objrouter: Router) { }
   login(frmLoginObj) {
     return this.objHTTP.post(this.urlLogin, frmLoginObj).pipe(map(res => res));
