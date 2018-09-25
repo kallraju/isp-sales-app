096e61cbf0d31122569b63a5edefe4061a95d12f
ramthulla9954 <ramthulla9866@gmail.com>
2018-09-25 16:27:31 +0530

    ramthulla
    service

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import { CanActivate, Router } from '@angular/router';
+
 @Injectable({
   providedIn: 'root'
 })
-export class ServiceService {
+export class ServiceService implements CanActivate {
+  urlLogin = 'http://103.263.168.73.3000/login';
 
-  constructor() { }
+  constructor(private objHTTP: HttpClient, private objrouter: Router) { }
+  login(frmLoginObj) {
+    return this.objHTTP.post(this.urlLogin, frmLoginObj).pipe(map(res => res));
+  }
+  canActivate() {
+    console.log(localStorage.getItem('U_Id' ));
+    if (localStorage.getItem('U_Id' ) !== null) {
+      this.objrouter.navigate(['']);
+      return false;
+    }
+    return true;
+  }
+
 }
