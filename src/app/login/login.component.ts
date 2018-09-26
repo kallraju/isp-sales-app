eafa22def26de81312e3a9be36c37fe073ceadcd
ramthulla9954 <ramthulla9866@gmail.com>
2018-09-26 09:51:45 +0530

    ramthulla
    see thisss

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormGroup,FormControl,Validators} from '@angular/forms';
+import { ServiceService } from '../service.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent  {
-frm: FormGroup;
+  Status: string;
+  lData: any = {};
+frm:FormGroup;
 username;
 password;
-  constructor() {
-    this.frm = new FormGroup({
-     un: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(5)]),
-     ps: new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(5)])
+  constructor(private objService:ServiceService,private objrouter:Router) { 
+    this.frm=new FormGroup({
+     un: new FormControl('',[Validators.required, Validators.minLength(2), Validators.maxLength(5)]),
+     ps: new FormControl('',[Validators.required, Validators.minLength(2), Validators.maxLength(5)])
     });
 
-    this.username = this.frm.controls['un'];
-    this.password = this.frm.controls['ps'];
+    this.username=this.frm.controls["un"];
+    this.password=this.frm.controls["ps"];
   }
-
+ 
 }
+
+ 
